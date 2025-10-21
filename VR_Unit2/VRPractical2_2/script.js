c6cec1b159758a48b17a27ca358252a7e6c385cb
Wanshui Ma <wanshuim@nycstudents.net>
2025-10-21 11:04:47 -0400

    FUckj the sun bro jk i love the sun

--- VR_Unit2/VRPractical2_2/script.js
+++ VR_Unit2/VRPractical2_2/script.js
@@ -1,5 +1,21 @@
 
 window.addEventListener("DOMContentLoaded",function() {
+    const sun = this.document.querySelector("#sun")
+    const car = this.document.querySelector("#car")
+    sun.setAttribute("opacity",0);
+
+    sun.fade = 1;
+    car.vx = -.112501;
+    sun.opacity = 0;
 
     
+    const loop = () => {
+        car.position = car.getAttribute("position");
+        sun.opacity+=sun.fade/100;
+        
+        car.setAttribute("position",{x:car.position.x+car.vx,y:car.position.y,z:car.position.z})
+        sun.opacity < 1 && sun.setAttribute("opacity", sun.opacity); 
+        this.requestAnimationFrame(loop);
+    }
+    loop();
 })
